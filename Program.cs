d6aafd831c45f18cb3f084bd93998b38ea4f9100
BinitBudwal <binitsainibudwal@student.mitt.ca>
2022-06-06 16:01:14 -0500

    Initial commit for Lab2

--- Program.cs
+++ Program.cs
@@ -1,55 +1,94 @@
-﻿Console.Write("Please enter a Number:");
-
-int integer = Int32.Parse(Console.ReadLine());
-
-Console.Write($"\nPlease enter {integer} words: \n");
-
-
-string[] wordsArray = new string[integer];
-for (int i = 0; i < wordsArray.Length; i++)
+﻿void Main()
 {
-    wordsArray[i] = Console.ReadLine();
-    string s = wordsArray[i] ;
-    bool hasSpace = s.Contains(" ");
-    if (hasSpace == true)
+    int GetNumberInput()
     {
-        //if this condition runs it breaks the code.
-        Console.WriteLine("Yor word contains space run the program again");
-        break;
+        Console.Write("Please enter a Number:");
 
+        int integer = Int32.Parse(Console.ReadLine());
+        return integer;
     }
-}
 
-Console.WriteLine("\n Please enter a character:");
+    int inputNumber = GetNumberInput();
 
-char character = Console.ReadKey().KeyChar;
-char lowerCharacter = char.ToLower(character);
-
-int counter = 0;
-int totalCharacters = 0;
-
-for (int i = 0; i < wordsArray.Length; i++)
-{
-    for (int j = 0; j < wordsArray[i].Length; j++)
+    string[] PopulateWordArray()
     {
-        if (wordsArray[i][j] == lowerCharacter)
+        Console.Write($"\nPlease enter {inputNumber} words: \n");
+        string[] wordsArray = new string[inputNumber];
+        for (int i = 0; i < wordsArray.Length; i++)
         {
-            counter++;
+            wordsArray[i] = Console.ReadLine();
+            string s = wordsArray[i];
+            bool hasSpace = s.Contains(" ");
+            if (hasSpace == true)
+            {
+                //if this condition runs it breaks the code.
+                Console.WriteLine("Yor word contains space run the program again");
+                break;
+
+            }
+        }
+        return wordsArray;
+    }
+
+    string[] stringsArray = PopulateWordArray();
+
+
+    char getCharacterInput()
+    {
+        Console.WriteLine("\n Please enter a character:");
+
+        char character = Console.ReadKey().KeyChar;
+        char lowerCharacter = char.ToLower(character);
+        return lowerCharacter;
+    }
+
+    char inputCharacter = getCharacterInput();
+
+    (int, int) CountCharacter()
+    {
+        int counter = 0;
+        int totalCharacters = 0;
+
+        for (int i = 0; i < stringsArray.Length; i++)
+        {
+            for (int j = 0; j < stringsArray[i].Length; j++)
+            {
+                if (stringsArray[i][j] == inputCharacter)
+                {
+                    counter++;
+
+                }
+                totalCharacters++;
+            }
+
 
         }
-        totalCharacters++;
+        return (counter, totalCharacters);
     }
 
+    var counterCount = CountCharacter();
+    //int totalCharacterCount = CountCharacter();
 
-}
+    double GetCharacterOccurrencePercentage()
+    {
+        double twentyFivePercent = 0.25 * counterCount.Item2;
+        return twentyFivePercent;
+    }
 
-double twentyFivePercent = 0.25 * totalCharacters;
+    double twentyFivePercentResult = GetCharacterOccurrencePercentage();
 
-if (counter > twentyFivePercent)
-{
-    Console.WriteLine($"\n The letter '{lowerCharacter}'appears {counter} times in the array.. This letter makes up more than 25% of the total number of characters.");
+    void PrintResults()
+    {
+        if (counterCount.Item1 > twentyFivePercentResult)
+        {
+            Console.WriteLine($"\n The letter '{inputCharacter}'appears {counterCount.Item1} times in the array.. This letter makes up more than 25% of the total number of characters.");
+        }
+        else
+        {
+            Console.WriteLine($"\n The letter '{inputCharacter}'appears {counterCount.Item1} times in the array. ");
+        }
+
+    }
+    PrintResults();
 }
-else
-{
-    Console.WriteLine($"\n The letter '{lowerCharacter}'appears {counter} times in the array. ");
-}
+Main();
